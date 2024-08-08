59c54388f96b955e79fdb6a4504ab4ce26767797
rdanielrs <daniel.r.sousa1911@gmail.com>
2024-08-08 14:03:46 -0300

    snake

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { SnakeComponent } from './snake/snake.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, SnakeComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'snake';
---
+++ src/app/snake/snake.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-snake',
+  standalone: true,
+  imports: [],
+  templateUrl: './snake.component.html',
+  styleUrl: './snake.component.scss'
+})
+export class SnakeComponent {
+}
