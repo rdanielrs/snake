0d2bb647f4456635b5cd35c0e69f7d2b62a157a5
rdanielrs <daniel.r.sousa1911@gmail.com>
2024-08-13 21:02:31 -0300

    .

--- src/app/snake/snake.component.ts
+++ src/app/snake/snake.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-snake',
@@ -8,4 +8,36 @@ import { Component } from '@angular/core';
   styleUrl: './snake.component.scss'
 })
 export class SnakeComponent {
+  width: string = "100px";
+
+  ngOnInit(): void {
+   
+    document.addEventListener('keydown', (e) => {
+      //console.log("Apertou essa porra")
+      this.keyHandler(e.key)
+    })
+  }
+
+  increaseWidth() {
+    let numericWidth = parseInt(this.width, 10) + 400;
+    this.width = numericWidth + "px"
+  }
+
+  keyHandler(key: string) {
+    console.log(key)
+    switch (key) {
+      case 'ArrowUp':
+          console.log("Arrow Up");
+          break;
+      case 'ArrowDown':
+          console.log('Arrow Down');
+          break;
+      case 'ArrowLeft':
+          console.log("Arrow Left");
+          break;
+      case 'ArrowRight':
+          console.log("Arrow Right");
+          break;
+    }
+  }
 }
